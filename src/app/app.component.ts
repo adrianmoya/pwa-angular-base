fa5bee626dd262230979064b2058f2156409fd1a
Adrian Moya <adrianmoya@gmail.com>
2018-09-27 20:26:50 -0500

    Adding subscribe button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa-angular-base';
+  pubObject = '';
+  VAPID_PUBLIC_KEY = 'BO3Ssq98k_Vbg2N6Jo0SDF10ZqgX20RMQ_wMhzXNp5yt91bt6AdsjBMTlwR4hOzKw2wyunNx1Ulzw6iIahTh6eU'
+
+  constructor(private swPush: SwPush) { }
+
+  subscribeToNotifications() {
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => {
+        console.log(sub.toJSON());
+        this.pubObject = JSON.stringify(sub.toJSON());
+      }
+      )
+      .catch(err => console.error("Could not subscribe to notifications", err));
+  }
 }
